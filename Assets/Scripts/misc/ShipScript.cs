8b0845abb4f69f838aa00d949707b33242da93a2
Fobiathegamer <fabiofanta07@gmail.com>
2024-05-15 00:03:44 +0200

    Iniziato giorno 4
    Aggiunte:
    - Nave
    - Alcune stanze
    
    Todo:
    - Fixare nave in Ocean0
    - Aggiungere cam shake
    - Aggiungere simone il salmone e la medusa con dialoghi

--- Assets/Scripts/misc/ShipScript.cs
+++ Assets/Scripts/misc/ShipScript.cs
@@ -1,19 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ShipScript : MonoBehaviour
 {
+    [SerializeField] float increment = 0.3f;
+    [SerializeField] float time = 10f;
+    float currentTime;
+    static ShipScript instance;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentTime = 0;
+        if (instance != null) Destroy(gameObject);
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(this);
+            SceneManager.activeSceneChanged += Deactivate;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        transform.position -= new Vector3(0, 0.3f, 0) * Time.deltaTime;
+        currentTime += Time.deltaTime;
+        if(currentTime <= time)
+            transform.position -= new Vector3(0, increment, 0) * Time.deltaTime;
+    }
+    private void Deactivate(Scene s1, Scene s2)
+    {
+        currentTime = time;
+        gameObject.transform.position = new Vector3(0, 0, 0);
     }
 }
